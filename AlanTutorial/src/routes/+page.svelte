5340059ef041837a739c322930cb9d866d8a0eb5
Seiya <jorisartist@gmail.com>
2026-01-14 14:20:13 +0100

    making tutorial, using Alantutorial as folder

---
+++ AlanTutorial/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+	import Nested from './second.svelte';
+</script>
+
+<Nested answer={42} />
---
+++ AlanTutorial/src/routes/second.svelte
@@ -0,0 +1,9 @@
+<script>
+	let { answer } = $props();
+</script>
+
+<p>The answer is {answer}</p>
+
+{#if answer === 42}
+	<p>The answer of the universe</p>
+{/if}
